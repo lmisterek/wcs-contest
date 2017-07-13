23cecee4cd4dc5fe295c1ca83f919194d1e5d001
LeeAnna Misterek <leeanna.misterek@gmail.com>
2017-07-13 16:13:27 -0700

    Changed package.json file.

--- server.js
+++ server.js
@@ -24,18 +24,10 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.use(require('./routes'));
 
-
-app.listen(1337, function() {
-	console.log('listening on port 1337');
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+	console.log('listening on port 3000');
 });
 
 
 
-
-// var http = require('http');
-
-// http.createServer(function(req, res) {
-// 	res.writeHead(200, {'Content-Type': 'text/plain'});
-// 	res.end('Hello World\n');
-// }).listen(1337, '127.0.0.1');
-// console.log('Server running at http://127.0.0.1:1337/');
