1e55e02184b3ea063fda9df7e30931be92da1cfa
LeeAnna Misterek <leeanna.misterek@gmail.com>
2017-08-07 19:42:46 -0700

    put foreign keys back in.

--- models/participant.js
+++ models/participant.js
@@ -14,7 +14,7 @@ module.exports = function(sequelize, DataTypes) {
 
   Participant.associate = function(models) {
     // Associating Participant with Scores
-    Participant.hasMany(models.Score);
+    Participant.hasMany(models.Score, {foreignKey: 'bib_number'});
   };
 
 
