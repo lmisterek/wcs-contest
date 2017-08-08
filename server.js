c9b85c6bc98317a6db8d2bb2797fa8901c048b54
LeeAnna Misterek <leeanna.misterek@gmail.com>
2017-08-07 21:02:07 -0700

    changed to drop.m

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.use('/contests', contests);
 
 // Syncing our sequelize models and then starting our Express app
 // ================================================================================
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 		console.log("App listening on PORT " + PORT);
 	});
