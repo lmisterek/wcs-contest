25ded3063ee83e694462b080bd5f8bff6471070a
James Wick <jmwick2@hotmail.com>
2017-08-07 23:55:33 -0700

    prelim models

--- models/participant.js
+++ models/participant.js
@@ -1,18 +1,30 @@
 'use strict';
 
 module.exports = function(sequelize, DataTypes) {
-    var Participant = sequelize.define('Participant', {
-        bib_number: DataTypes.STRING,
-        lastname: DataTypes.STRING,
-        firstname: DataTypes.STRING,
-        division: DataTypes.STRING,
-        role: DataTypes.STRING
-    });
+  var Participant = sequelize.define('Participant', {
+    bib_number: {
+      type:  DataTypes.STRING,
+      primaryKey: true,
+      underscored: true
+    },
+    lastname: DataTypes.STRING,
+    firstname: DataTypes.STRING,
+    division: DataTypes.STRING,
+    role: DataTypes.STRING
+  });
 
-    Participant.associate = function(models) {
-        // Associating Participant with Scores
-        Participant.hasMany(models.Score, { foreignKey: 'bib_number' });
-    };
+  Participant.associate = function(models) {
+    // Associating Participant with Scores
+    Participant.hasMany(models.Score, {foreignKey: "bib_number"});
+  };
 
-    return Participant;
+
+  return Participant;
 };
+
+
+
+
+
+
+
