6b8689726c281924d5e22ebda036dfe8be9118e3
James Wick <jmwick2@hotmail.com>
2017-09-16 16:40:29 -0700

    server.js save

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
 var expressValidator = require('express-validator');
 var flash = require('connect-flash');
 var session = require('express-session');
